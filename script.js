1c30782d2572047dc013cc4450e6e90ceaa51e0b
Tom <thomasbeckenhauer+cloud9@gmail.com>
2016-05-04 02:48:26 +0000

    add ability to swap between basic controls and bootstrap controls

--- script.js
+++ script.js
@@ -27,181 +27,220 @@ angular.module('carApp')
       scope: {
         msgArray: '='
       },
-      template: '<div ng-repeat="msg in msgArray track by $index">' +
-        '<div ng-class="msg.color">{{msg.message}}</div>' +
+      template: '<div>' +
+        '<div ng-repeat="msg in msgArray track by $index" ng-class="msg.color">{{msg.message}}</div>' +
         '</div>'
     }
   }])
-  .controller('carController', ['$scope', 'carStateMachine', function($scope, carState) {
-    $scope.logObj = [];
-    $scope.updateLog = function(logArray) {
-      var logToMessageMap = {
-        info: 'blue',
-        warn: 'orange',
-        error: 'red'
-      }
-      $scope.messageArray = logArray.map(function(logObject) {
-        return {
-          color: logToMessageMap[logObject.level],
-          message: logObject.item,
-        }
-      });
+
+.controller('carController', ['$scope', 'carStateMachine', function($scope, carState) {
+  $scope.logObj = [];
+  $scope.updateLog = function(logArray) {
+    var logToMessageMapStandard = {
+      info: 'blue',
+      warn: 'orange',
+      error: 'red'
     }
-    carState().addHandler($scope.updateLog);
-    $scope.acceptCommand = function(command) {
-      var commandMap = {
-        startEngine: 'insertKeys',
-        turnOffEngine: 'removeKeys',
-        pushForward: 'accelerate',
-        pullBack: 'deAccelerate'
+    var logToMessageMapBootstrap = {
+      info: 'list-group-item-info',
+      warn: 'list-group-item-warning',
+      error: 'list-group-item-danger'
+    }
+
+    var logToMessageMap = $scope.isBootstrap ? logToMessageMapBootstrap : logToMessageMapStandard;
+    
+    $scope.messageArray = logArray.map(function(logObject) {
+      return {
+        color: logToMessageMap[logObject.level],
+        message: logObject.item,
       }
-      carState().handleInput(commandMap[command]);
+    });
+  }
+  carState().addHandler($scope.updateLog);
+  $scope.acceptCommand = function(command) {
+    var commandMap = {
+      startEngine: 'insertKeys',
+      turnOffEngine: 'removeKeys',
+      pushForward: 'accelerate',
+      pullBack: 'deAccelerate'
+    }
+    carState().handleInput(commandMap[command]);
+  }
+}])
+
+.config(['carStateMachineProvider', function(carStateMachineProvider) {
+
+  carStateMachineProvider.registerState('parked', {
+    handleInput: {
+      value: function(command) {
+        switch (command) {
+          case 'accelerate':
+            this.warn(command);
+            break;
+          case 'deAccelerate':
+            this.warn(command);
+            break;
+          case 'removeKeys':
+            this.info();
+            break;
+          case 'insertKeys':
+            this.set('stopped');
+            break;
+        }
+      }
+    }
+  }, true);
+
+  carStateMachineProvider.registerState('stopped', {
+    handleInput: {
+      value: function(command) {
+        switch (command) {
+          case 'accelerate':
+            this.set('driving');
+            break;
+          case 'deAccelerate':
+            this.set('reverse');
+            break;
+          case 'removeKeys':
+            this.set('parked');
+            break;
+          case 'insertKeys':
+            this.info();
+            break;
+        }
+      }
+    }
+  });
+
+  carStateMachineProvider.registerState('driving', {
+    handleInput: {
+      value: function(command) {
+        switch (command) {
+          case 'accelerate':
+            this.info();
+            break;
+          case 'deAccelerate':
+            this.set('stopped');
+            break;
+          case 'removeKeys':
+            this.warn(command);
+            break;
+          case 'insertKeys':
+            this.warn(command);
+            break;
+        }
+      }
+    }
+  });
+
+  carStateMachineProvider.registerState('reverse', {
+    handleInput: {
+      value: function(command) {
+        switch (command) {
+          case 'accelerate':
+            this.set('stopped');
+            break;
+          case 'deAccelerate':
+            this.info();
+            break;
+          case 'removeKeys':
+            this.warn(command);
+            break;
+          case 'insertKeys':
+            this.warn(command);
+            break;
+        }
+      }
+    }
+  });
+
+}])
+
+.provider('carStateMachine', [function() {
+
+  var protoVehical = function() {}
+  var carLog = [];
+  var vehicalStates = {};
+  var getCurrentState = function() {
+    return vehicalStates[currentState];
+  }
+  var setCurrentState = function(state) {
+    currentState = state;
+
+  }
+  var handlers = [];
+
+  protoVehical.prototype.set = function(newState) {
+    var newMessage = 'setting vehical to state:' + newState;
+    setCurrentState(newState);
+    this.log({
+      level: 'info',
+      item: newMessage
+    });
+  }
+
+  protoVehical.prototype.warn = function(cmd) {
+    var newMessage = 'command: ' + cmd + ' in state: ' + currentState + ' is not allowed';
+    this.log({
+      level: 'warn',
+      item: newMessage
+    });
+  }
+
+  protoVehical.prototype.info = function() {
+    var newMessage = 'remaining in state:' + currentState;
+    this.log({
+      level: 'info',
+      item: newMessage
+    });
+  }
+
+  protoVehical.prototype.log = function(newMessage) {
+    carLog.unshift(newMessage);
+    handlers.forEach(function(handler) {
+      handler(carLog);
+    });
+  }
+
+  protoVehical.prototype.addHandler = function(newHandler) {
+    handlers.push(newHandler);
+  };
+
+  this.registerState = function(state, stateConfig, isDefault) {
+    vehicalStates[state] = Object.create(protoVehical.prototype, stateConfig);
+    if (isDefault) setCurrentState(state);
+  }
+
+  this.$get = [function() {
+    return getCurrentState;
+  }];
+}])
+
+.directive('dashboardButtonBootstrap', [function() {
+    return {
+      replace: true,
+      template: '<button type="button" class="btn btn-default" ng-click="execCmd()">{{cmdVal}}</button>',
+      scope: {
+        execCmd: '&',
+        cmdVal: '@',
+      }
     }
   }])
-  .config(['carStateMachineProvider', function(carStateMachineProvider) {
-
-    carStateMachineProvider.registerState('parked', {
-      handleInput: {
-        value: function(command) {
-          switch (command) {
-            case 'accelerate':
-              this.warn(command);
-              break;
-            case 'deAccelerate':
-              this.warn(command);
-              break;
-            case 'removeKeys':
-              this.info();
-              break;
-            case 'insertKeys':
-              this.set('stopped');
-              break;
-          }
-        }
-      }
-    }, true);
-
-    carStateMachineProvider.registerState('stopped', {
-      handleInput: {
-        value: function(command) {
-          switch (command) {
-            case 'accelerate':
-              this.set('driving');
-              break;
-            case 'deAccelerate':
-              this.set('reverse');
-              break;
-            case 'removeKeys':
-              this.set('parked');
-              break;
-            case 'insertKeys':
-              this.info();
-              break;
-          }
-        }
-      }
-    });
-
-    carStateMachineProvider.registerState('driving', {
-      handleInput: {
-        value: function(command) {
-          switch (command) {
-            case 'accelerate':
-              this.info();
-              break;
-            case 'deAccelerate':
-              this.set('stopped');
-              break;
-            case 'removeKeys':
-              this.warn(command);
-              break;
-            case 'insertKeys':
-              this.warn(command);
-              break;
-          }
-        }
-      }
-    });
-
-    carStateMachineProvider.registerState('reverse', {
-      handleInput: {
-        value: function(command) {
-          switch (command) {
-            case 'accelerate':
-              this.set('stopped');
-              break;
-            case 'deAccelerate':
-              this.info();
-              break;
-            case 'removeKeys':
-              this.warn(command);
-              break;
-            case 'insertKeys':
-              this.warn(command);
-              break;
-          }
-        }
-      }
-    });
-
+  .directive('dashboardBootstrap', [function() {
+    return {
+      replace: true,
+      transclude: true,
+      template: '<div class="btn-group" role="group" ng-transclude></div>',
+    }
   }])
-  .provider('carStateMachine', [function() {
-
-    var protoVehical = function() {}
-    var carLog = [];
-    var vehicalStates = {};
-    var getCurrentState = function() {
-      return vehicalStates[currentState];
+  .directive('messagesBootstrap', [function() {
+    return {
+      replace: true,
+      scope: {
+        msgArray: '='
+      },
+      template: '<ul class="list-group">' +
+        '<li class="list-group-item" ng-class="msg.color" ng-repeat="msg in msgArray track by $index">{{msg.message}}</li>' +
+        '</ul>'
     }
-    var setCurrentState = function(state) {
-      currentState = state;
-
-    }
-    var handlers = [];
-
-    protoVehical.prototype.set = function(newState) {
-      var newMessage = 'setting vehical to state:' + newState;
-      setCurrentState(newState);
-      this.log({
-        level: 'info',
-        item: newMessage
-      });
-    }
-
-    protoVehical.prototype.warn = function(cmd) {
-      var newMessage = 'command: ' + cmd + ' in state: ' + currentState + ' is not allowed';
-      this.log({
-        level: 'warn',
-        item: newMessage
-      });
-    }
-
-    protoVehical.prototype.info = function() {
-      var newMessage = 'remaining in state:' + currentState;
-      this.log({
-        level: 'info',
-        item: newMessage
-      });
-    }
-
-    protoVehical.prototype.log = function(newMessage) {
-      carLog.push(newMessage);
-      handlers.forEach(function(handler) {
-        handler(carLog);
-      });
-    }
-
-
-    protoVehical.prototype.addHandler = function(newHandler) {
-      handlers.push(newHandler);
-    };
-
-    this.registerState = function(state, stateConfig, isDefault) {
-      vehicalStates[state] = Object.create(protoVehical.prototype, stateConfig);
-      if (isDefault) setCurrentState(state);
-    }
-
-    this.$get = [function() {
-      return getCurrentState;
-    }];
   }])
