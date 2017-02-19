ea1c99188bcc7bd3a563bb4b1e09f042fc170498
Thomas Beckenhauer <tom@beckenhauer.me>
2017-02-18 20:37:34 -0500

    issolate ourselves from third party libraries
    fix
    
    fix

--- script.js
+++ script.js
@@ -1,61 +1,100 @@
 angular.module('carApp', []);
 
 angular.module('carApp')
-  .directive('dashboardButton', [function() {
+    .directive('tmbDashboard', ['settings', function (settings) {
+        return {
+            controller: function ($scope) {
+                settings.listenTo('uistyle', function(uistyle) {
+                    $scope.uistyle = uistyle;
+                });
+            },
+            replace: true,
+            transclude: true,
+            //I am cheating a little bit here. These are bootstrap specific properties.
+            //Basically the logic got very hairy with an ng-switch inside of a transclude inside of an ng-switch
+            //A lot of things could be simplified if the uisettings had been decided on by the configure stage.
+            //Instead I will just swap out css classes as necessary.
+            template: '<div ng-transclude class="btn-group" role="group"></div>',
+            scope: {
+                executeCommand: '&'
+            }
+        }
+    }])
+  .directive('tmbDashboardButton', ['settings', function (settings) {
     return {
+      controller: function ($scope) {
+        settings.listenTo('uistyle', function(uistyle) {
+          $scope.uistyle = uistyle;
+        });
+      },
       replace: true,
-      template: '<input type="button" ng-click="execCmd()" value="{{cmdVal}}">',
+        template: ''+
+        '<ng-switch on="uistyle">' +
+            '<button ng-switch-when="bootstrap" type="button" class="btn btn-default" ng-click="execCmd()">{{cmdVal}}</button>'+
+            '<input ng-switch-default type="button" ng-click="execCmd()" value="{{cmdVal}}">'+
+        '</ng-switch>',
       scope: {
         execCmd: '&',
-        cmdVal: '@',
+        cmdVal: '@'
       }
     }
   }])
-  .directive('dashboard', [function() {
+  .directive('tmbMessages', ['settings', function (settings) {
     return {
+      controller: function ($scope) {
+        settings.listenTo('uistyle', function(uistyle) {
+          $scope.uistyle = uistyle;
+        });
+      },
       replace: true,
-      transclude: true,
-      template: '<div ng-transclude></div>',
-      scope: {
-        executeCommand: '&'
-      }
-    }
-  }])
-  .directive('messages', [function() {
-    return {
-      replace: true,
+      template: ''+
+      '<ng-switch on="uistyle">'+
+          '<ul ng-switch-when="bootstrap" class="list-group">' +
+            '<li class="list-group-item" ng-class="msg.color" ng-repeat="msg in msgArray track by $index">{{msg.message}}</li>' +
+          '</ul>'+
+          '<div ng-switch-default>' +
+            '<div ng-repeat="msg in msgArray track by $index" ng-class="msg.color">{{msg.message}}</div>' +
+          '</div>'+
+      '</ng-switch>',
       scope: {
         msgArray: '='
-      },
-      template: '<div>' +
-        '<div ng-repeat="msg in msgArray track by $index" ng-class="msg.color">{{msg.message}}</div>' +
-        '</div>'
+      }
     }
   }])
 
-.controller('carController', ['$scope', 'carStateMachine', function($scope, carState) {
+.controller('carController', ['$scope', 'carStateMachine', 'settings', function($scope, carState, settings) {
+
+  $scope.updateUiStyle = function (isBootstrap) {
+      if(isBootstrap) {
+          settings.uistyle = 'bootstrap';
+      } else {
+          settings.uistyle = 'vanilla';
+      }
+  };
+
   $scope.logObj = [];
   $scope.updateLog = function(logArray) {
     var logToMessageMapStandard = {
       info: 'blue',
       warn: 'orange',
       error: 'red'
-    }
+    };
     var logToMessageMapBootstrap = {
       info: 'list-group-item-info',
       warn: 'list-group-item-warning',
       error: 'list-group-item-danger'
-    }
+    };
 
     var logToMessageMap = $scope.isBootstrap ? logToMessageMapBootstrap : logToMessageMapStandard;
-    
+
     $scope.messageArray = logArray.map(function(logObject) {
       return {
         color: logToMessageMap[logObject.level],
         message: logObject.item,
       }
     });
-  }
+  };
+
   carState().addHandler($scope.updateLog);
   $scope.acceptCommand = function(command) {
     var commandMap = {
@@ -63,11 +102,48 @@ angular.module('carApp')
       turnOffEngine: 'removeKeys',
       pushForward: 'accelerate',
       pullBack: 'deAccelerate'
-    }
+    };
     carState().handleInput(commandMap[command]);
   }
 }])
 
+    .service('settings', function () {
+        //settings will be an observable, that is you can register a callback with
+        //listenTo, and it will notify you when a property has been changed
+        var self = this;
+
+        //This is the private storage of properties, and their associated callbacks
+        var settings = {
+            uistyle: {
+                value: 'vanilla',
+                callbacks : []
+            }
+        };
+
+        //Walk through each of the properies on settings, and define
+        // a getter/setter for it.  When set, notify the listeners.
+        Object.keys(settings).forEach(function(key) {
+            Object.defineProperty(self, key, {
+                set: function(value) {
+                    //update the property with the new value
+                    settings[key].value = value;
+                    //walk through all the callbacks for the property and call each callback
+                    settings[key].callbacks.forEach(function(callback) {
+                        callback(value);
+                    });
+                },
+                get: function() {
+                    return settings[key];
+                }
+            });
+        });
+
+        self.listenTo = function (property, callback) {
+            //look up the callbacks of the property and add a new callback.
+            settings[property].callbacks.push(callback);
+        };
+        return self;
+    })
 .config(['carStateMachineProvider', function(carStateMachineProvider) {
 
   carStateMachineProvider.registerState('parked', {
@@ -215,32 +291,3 @@ angular.module('carApp')
     return getCurrentState;
   }];
 }])
-
-.directive('dashboardButtonBootstrap', [function() {
-    return {
-      replace: true,
-      template: '<button type="button" class="btn btn-default" ng-click="execCmd()">{{cmdVal}}</button>',
-      scope: {
-        execCmd: '&',
-        cmdVal: '@',
-      }
-    }
-  }])
-  .directive('dashboardBootstrap', [function() {
-    return {
-      replace: true,
-      transclude: true,
-      template: '<div class="btn-group" role="group" ng-transclude></div>',
-    }
-  }])
-  .directive('messagesBootstrap', [function() {
-    return {
-      replace: true,
-      scope: {
-        msgArray: '='
-      },
-      template: '<ul class="list-group">' +
-        '<li class="list-group-item" ng-class="msg.color" ng-repeat="msg in msgArray track by $index">{{msg.message}}</li>' +
-        '</ul>'
-    }
-  }])
